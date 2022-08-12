f1851f4cf486d2851fc5e9b6db846be6907df73f
Vagner Silva Santos <vagner-se@hotmail.com>
2022-08-12 10:09:35 -0300

    first commit project Hamburgueria

---
+++ README.md
@@ -0,0 +1,40 @@
+#Projeto Hamburgueria - back-end
+
+
+
+<img src="./img/result1.png" width="300px" alt="img">
+
+<img src="./img/result2.png" width="300px" alt="img-">
+
+
+
+> Resultado do Projeto Hamburgueria
+
+### Sobre:
+
+O projeto é uma aplicação back-end usando javascript, node, express
+
+- [x] EXPRESS
+- [X] NODE
+- [X] JAVASCRIPT
+- [X] INSOMNIA
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr>
+    <td aling="center">
+      <a href="#">
+        <img src="./img/vagner.jpg" width="100px;" alt="Foto do Vagner"/><br>
+        <sub>
+          <b>Vagner Santos</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+
+
---
+++ index.js
@@ -0,0 +1,105 @@
+
+const express = require('express')
+const uuid = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+const users = []
+
+const checkUser = (request, response, next) =>{
+
+    const {id}= request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({message: "User Not Found"})
+        
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+
+}
+
+const checkUrl = (request, response, next) =>{
+
+    const method = request.route.methods
+    const url = request.route.path
+    console.log(method, url)
+
+    next()
+
+}
+
+app.get('/order/', checkUrl,(request, response) =>{
+
+        return response.json(users)
+})
+
+app.get('/order/:id', checkUser, checkUrl,(request, response) =>{
+
+    
+    const index = request.userIndex
+    const id = request.userId
+
+
+   users[index, id]
+
+    
+    return response.json(users)
+})
+
+app.post('/order/', checkUrl, (request, response) =>{
+
+    const {id, order,clientName, price, status} = request.body
+
+    const user = {id: uuid.v4(), order,clientName, price, status}
+
+    users.push(user)
+
+    
+    return response.status(201).json(users)
+})
+
+app.put('/order/:id', checkUser, checkUrl, (request, response) =>{
+
+    const {order, clientName, price, status} = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updateUsers = {id, order,clientName, price, status}
+
+   users[index] = updateUsers
+
+    
+    return response.json(updateUsers)
+})
+
+app.delete('/order/:id', checkUser, checkUrl, (request, response) =>{
+
+    const index = request.userIndex
+
+    users.splice(index, 1)
+
+    return response.status(204).json(users)
+})
+
+app.patch('/order/:id', checkUser, checkUrl, (request, response) =>{
+    const index = request.userIndex
+        
+   
+
+   users[index].status = "Pronto"
+
+    return response.json(users[index])
+})
+
+app.listen(port, () => {
+    console.log(`🌎Server started on port ${port}`)
+})
